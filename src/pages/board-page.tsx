f10b39f28a588d499ea6c16585cc6ef29cd6ccd5
Barak Kaldess <ibarakkaldess@gmail.com>
2023-01-16 15:42:19 +0200

    boardpage - mobile responsive and UI design

--- src/pages/board-page.tsx
+++ src/pages/board-page.tsx
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react"
+import { getWords } from "../services/words-service"
+
+type TeamTurn = 'red' | 'blue'
+interface Word {
+    word: string
+    isShown: boolean
+    color: string
+}
+
 export const BoardPage = () => {
+    const [teamTurn, setTeamTurn] = useState<TeamTurn>('red')
+    const [words, setWords] = useState<Word[]>([])
+
+    useEffect(() => {
+        setWords(getWords())
+    },[])
+
+
     return (
-        <div>
-            <h1>Hello From Board</h1>
+        <div className="board-page">
+            <h1 className="text-center">CODENAMES</h1>
+            <div className="board-status flex space-between align-center">
+                <h2><span className="red-text">9</span> - <span className="blue-text">8</span></h2>
+                <h2 className={(teamTurn === 'red') ? 'red-text' : 'blue-text'}>{teamTurn}'s turn</h2>
+                <button className="end-turn-btn">End {teamTurn}'s Turn</button>
+            </div>
+            <div className="board-words">
+                {words.map((word, idx) => {
+                    return <div className="word-item" key={idx}><h3>{word.word}</h3></div>
+                })}
+            </div>
+            <div className="board-options flex">
+                <p>join room: ASDF</p>
+                <div className="board-options-btns">
+                    <button>Players</button>
+                    <button>Spy-master</button>
+                    <button>Next Game</button>
+                </div>
+            </div>
         </div>
     )
 }
+
---
+++ src/services/words-service.ts
@@ -0,0 +1,107 @@
+
+export const getWords = () => {
+    return demoWords
+}
+
+const demoWords = [
+    {word: 'abc',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'wabc',
+    isShown: false,
+    color: 'none'
+},
+    {word: 'aabc',
+    isShown: false,
+    color: 'none'
+},
+    {word: 'aabc',
+    isShown: false,
+    color: 'none'
+},
+    {word: 'ajbc',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'anbc',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'aybc',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'arbc',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'abbc',
+    isShown: false,
+    color: 'none'
+},
+    {word: 'acbc',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'axbc',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'abzc',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'abac',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'absc',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'absc',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'abcn',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'abcb',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'abcy',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'abct',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'abcq',
+    isShown: false,
+    color: 'blue'
+},
+    {word: 'abcw',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'abca',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'abc',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'abcf',
+    isShown: false,
+    color: 'red'
+},
+    {word: 'abcs',
+    isShown: false,
+    color: 'red'
+},
+]
